be966fe5d3de4fa8e981874a6bb1a2b3c5a58d9e
mota <thiagomotape@hotmail.com.br>
2023-04-09 14:48:01 -0300

    update
    deixando o background da tela preto

--- ResidentEvil/src/com/studio/main/Game.java
+++ ResidentEvil/src/com/studio/main/Game.java
@@ -1,10 +1,36 @@
 package com.studio.main;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
 public class Game extends Canvas implements  Runnable {
 
     private boolean isRunning = true;
+    public static JFrame frame;
+    private BufferedImage image;
     private Thread thread;
+    private  final int WIDTH = 240;
+    private final int HEIGHT = 160;
+    private final int SCALE = 3;
+
+
+    public Game() {
+        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        initFrame();
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    }
+
+    public void initFrame() {
+        frame = new JFrame("RESIDENT EVIL 2D");
+        frame.add(this);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
     public synchronized void start() {
         thread = new Thread(this);
@@ -18,7 +44,6 @@ public class Game extends Canvas implements  Runnable {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
     }
     public static void main(String[] args) {
         Game game = new Game();
@@ -31,6 +56,20 @@ public class Game extends Canvas implements  Runnable {
     }
     public void render() {
         //graficos do game
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = image.getGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+
+        g.dispose();
+        g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+        bs.show();
     }
 
     @Override
