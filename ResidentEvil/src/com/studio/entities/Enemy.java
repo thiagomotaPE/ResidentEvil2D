cb650353f1ad766e2586312cfce65a3298538785
mota <thiagomotape@hotmail.com.br>
2023-04-26 18:19:23 -0300

    enemyspeed
    adicionei o movimento ao inimigo e fiz a colisão deles com o mapa

--- ResidentEvil/src/com/studio/entities/Enemy.java
+++ ResidentEvil/src/com/studio/entities/Enemy.java
@@ -1,10 +1,27 @@
 package com.studio.entities;
 
+import com.studio.main.Game;
+import com.studio.world.World;
+
 import java.awt.image.BufferedImage;
 
 public class Enemy extends Entity{
+    private double speed = 0.4;
 
     public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
         super(x, y, width, height, sprite);
+    }
+
+    public void tick() {
+        if((int)x < Game.player.getX() && World.isFree((int)(x + speed), this.getY()))
+            x += speed;
+        if((int)x > Game.player.getX() && World.isFree((int)(x - speed), this.getY()))
+            x -= speed;
+
+        if((int)y < Game.player.getY() && World.isFree(this.getX(), (int)(y + speed)))
+            y += speed;
+        if((int)y > Game.player.getY() && World.isFree(this.getX(), (int)(y - speed)))
+            y -= speed;
+
     }
 }
--- ResidentEvil/src/com/studio/main/Game.java
+++ ResidentEvil/src/com/studio/main/Game.java
@@ -110,6 +110,7 @@ public class Game extends Canvas implements  Runnable, KeyListener {
         double delta = 0;
         int frames = 0;
         double timer = System.currentTimeMillis();
+        requestFocus();
 
         while(isRunning) {
             long now = System.nanoTime();
