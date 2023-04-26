a74c8625cca0a095d6fcb626f8e1aa312da5c614
mota <thiagomotape@hotmail.com.br>
2023-04-26 17:56:17 -0300

    collision
    fiz a colisão do mapa

--- ResidentEvil/src/com/studio/world/World.java
+++ ResidentEvil/src/com/studio/world/World.java
@@ -8,9 +8,12 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import static com.studio.main.Game.world;
+
 public class World {
-    private Tile[] tiles;
+    private static Tile[] tiles;
     public static int WIDTH, HEIGHT;
+    public static final int TILE_SIZE = 16;
 
     public World(String path) {
         try {
@@ -30,7 +33,7 @@ public class World {
                         tiles[xx + (yy * WIDTH)] = new FloorTile(xx*16, yy*16, Tile.TILE_FLOOR);
                     }else if(pixelAtual == 0xFFFFFFFF) {
                         //wall
-                        tiles[xx + (yy * WIDTH)] = new FloorTile(xx*16, yy*16, Tile.TILE_WALL);
+                        tiles[xx + (yy * WIDTH)] = new WallTile(xx*16, yy*16, Tile.TILE_WALL);
                     }else if(pixelAtual == 0xFF0000FF) {
                         //player
                         Game.player.setX(xx*16);
@@ -53,6 +56,26 @@ public class World {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+    public static boolean isFree(int xnext, int ynext) {
+        int x1 = xnext / TILE_SIZE;
+        int y1 = ynext / TILE_SIZE;
+
+        int x2 =( xnext + TILE_SIZE - 1) / TILE_SIZE;
+        int y2 = ynext / TILE_SIZE;
+
+        int x3 = xnext / TILE_SIZE;
+        int y3 = (ynext + TILE_SIZE - 1) / TILE_SIZE;;
+
+        int x4 = (xnext + TILE_SIZE - 1) / TILE_SIZE;
+        int y4 = (ynext + TILE_SIZE - 1) / TILE_SIZE;
+
+        return !((tiles[x1 + (y1 * world.WIDTH)] instanceof WallTile) ||
+                (tiles[x2 + (y2 * world.WIDTH)] instanceof WallTile) ||
+                (tiles[x3 + (y3 * world.WIDTH)] instanceof WallTile) ||
+                (tiles[x4 + (y4 * world.WIDTH)] instanceof WallTile));
 
     }
     public void render(Graphics g) {
